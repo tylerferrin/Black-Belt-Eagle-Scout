f3a429584471b3c4683daf898f2cfc6c5d48f35b
Tyler Ferrin <tyferrin@gmail.com>
2018-05-16 16:38:48 -0700

    add KP favicon to static file and implement

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'Band website' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/kp.png' }
     ]
   },
   /*
